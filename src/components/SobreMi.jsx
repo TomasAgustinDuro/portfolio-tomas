b5556b0c841c322fef4a62d383a0950f8f347684
Tomas <durotomaas@gmail.com>
2024-07-28 22:01:14 -0300

    correcciones a textos

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -14,18 +14,18 @@ export function SobreMi() {
 
           <div>
             <p>
-              Me llamo Tomas, soy de Buenos Aires, Argentina. Comence a estudiar
-              desarrollo web mediante cursos hasta entrar en la Tecnicatura de
-              Desarrollo web y Aplicaciones Moviles que dictaba el ISPC. A
-              partir de ahi estoy desarrollando diversos proyectos tanto
-              personales como del estudio que me permiten ir avanzando con mi
-              carrera profesional.
+              Me llamo Tomás, soy de Buenos Aires, Argentina. Comencé a estudiar
+              desarrollo web mediante cursos hasta ingresar en la Tecnicatura de
+              Desarrollo Web y Aplicaciones Móviles que dicta el ISPC. A partir
+              de ahí, estoy desarrollando diversos proyectos tanto personales
+              como del estudio, lo que me permite avanzar en mi carrera
+              profesional.
               <br />
               <br />
-              Ahora mismo estoy incursionando en el freelance aunque tambien
-              estoy interesando en trabajar bajo dependencia. Mi objetivo es
-              poder vivir de esta pasion que es la creacion de paginas webs,
-              hacer que mi hobbie sea mi trabajo lo cual considero que es muy
+              Ahora mismo estoy incursionando en el freelance, aunque también
+              estoy interesado en trabajar bajo dependencia. Mi objetivo es
+              poder vivir de esta pasión que es la creación de páginas web,
+              hacer que mi hobby sea mi trabajo, lo cual considero que es muy
               importante para la salud mental de uno mismo.
             </p>
           </div>

0cdad48c8071fecafb9d5c6e3a6bfab2de8a3d8a
Tomas <durotomaas@gmail.com>
2025-03-12 17:45:47 -0300

    Add animation to about me section

--- src/components/sobre-mi/SobreMi.jsx
+++ src/components/sobre-mi/SobreMi.jsx
@@ -2,13 +2,24 @@ import fotoCv from "/img/fotoCv.png";
 import { IoPersonOutline } from "react-icons/io5";
 import styles from "./sobreMi.module.css";
 import { useTranslation } from "react-i18next";
+import {motion} from "framer-motion";
 
 export function SobreMi() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+  const maskVariants = {
+    hidden: { clipPath: "inset(0 100% 0 0)" }, // Oculta el contenido (máscara al 100% a la derecha)
+    reveal: { clipPath: "inset(0 0% 0 0)" }, // Revela el contenido (máscara al 0%)
+  };
 
   return (
     <>
-      <section className={styles.sobreMi}>
+      <motion.section
+        className={styles.sobreMi}
+        initial="hidden"
+        whileInView="reveal"
+        variants={maskVariants}
+        transition={{ duration: 1.5, ease: "easeInOut" }}
+      >
         <h2>
           <IoPersonOutline /> {t("aboutMe.title")}
         </h2>
@@ -25,7 +36,7 @@ export function SobreMi() {
             </p>
           </div>
         </main>
-      </section>
+      </motion.section>
     </>
   );
 }
